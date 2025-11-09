0ef89f2070fa89b2fcdea9ca3225d4d60201f432
LucasFDutra <lucasfelipedutra@gmail.com>
2025-11-09 10:04:54 -0300

    [RUST] sublist - implementação melhorada

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,28 +6,28 @@ pub enum Comparison {
     Unequal,
 }
 
+fn is_sublist(smaller: &[i32], larger: &[i32]) -> bool {
+    if smaller.len() == 0 {
+        return true;
+    }
+    for chunk in larger.windows(smaller.len()) {
+        if chunk == smaller {
+            return true;
+        }
+    }
+    return false;
+}
+
 pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
     if first_list == second_list {
         return Comparison::Equal;
-    } else if first_list.len() == 0 {
-        return Comparison::Sublist;
-    } else if second_list.len() == 0 {
-        return Comparison::Superlist;
     } else if first_list.len() < second_list.len() {
-        let len_first = first_list.len();
-        let n_chunks = second_list.len() - len_first + 1;
-        for i in 0..n_chunks {
-            if &second_list[i..i + len_first] == first_list {
-                return Comparison::Sublist;
-            }
+        if is_sublist(first_list, second_list) {
+            return Comparison::Sublist;
         }
     } else if first_list.len() > second_list.len() {
-        let len_second = second_list.len();
-        let n_chunks = first_list.len() - len_second + 1;
-        for i in 0..n_chunks {
-            if &first_list[i..i + len_second] == second_list {
-                return Comparison::Superlist;
-            }
+        if is_sublist(second_list, first_list) {
+            return Comparison::Superlist;
         }
     }
 
